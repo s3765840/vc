1d72a06f37418cd90801549c5bbfac7010158124
s3765840 <s3765840@student.rmit.edu.au>
2021-10-17 05:28:31 +1300

    Post can save

--- src/components/PostPage/Post.js
+++ src/components/PostPage/Post.js
@@ -1,23 +1,30 @@
 import React, { useState, useEffect } from "react";
 import "./Post.css";
+import axios from "axios";
 
 function Post() {
   const [detail, setDetail] = useState({
+    id:0,
     name: "",
     email: "",
     password: "",
     confirmPassword: "",
-    creatAt: "",
   });
-  const [postData, setPostData] = useState({ email: "", post: "" });
+  const [post, setPost] = useState({
+    content: "",
+    user_id: "",
+    image: "",
+  });
+  // const [postData, setPostData] = useState({ email: "", post: "" });
   const [postDetail, setPostDetail] = useState("");
   const [postSuccessMeg, setPostSuccessMeg] = useState("");
+  const [response, setResponse] = useState([]);
 
   const [postError, setPostError] = useState("");
   const [isCorrect, setIsCorrect] = useState(false);
   const [isClickSubmit, setIsClickSubmit] = useState(false);
   const [postsList, setPostsList] = useState([]);
-  const [isLocalStorageEmpty, setIsLocalStorageEmpty] = useState(true);
+  // const [isLocalStorageEmpty, setIsLocalStorageEmpty] = useState(true);
 
   const formValidation = () => {
     var isValid = "true";
@@ -35,52 +42,75 @@ function Post() {
   }, []);
   useEffect(() => {
     if (isClickSubmit) {
-      setPostData({ email: detail.email, post: postDetail });
+      // setPostData({ email: detail.email, post: postDetail });
+      setPost({user_id:detail.id,content:postDetail})
+      console.log(detail);
     }
   }, [isCorrect]);
   useEffect(() => {
     if (isClickSubmit) {
-      console.log(postData);
-      creatOrFindStorage();
-    }
-  }, [postData]);
-  useEffect(() => {
-    if (isClickSubmit) {
+      console.log(post);
+      axios
+      .post("http://localhost:5001/post/addPost", post)
+      .then((response) => setResponse(response.data));
       updateStorage();
     }
-  }, [postsList]);
+  }, [post]);
+  useEffect(() => {
+    if (isClickSubmit) {
+      console.log(response);
+    }
+  }, [response]);
+  // useEffect(() => {
+  //   if (isClickSubmit) {
+  //     console.log(postData);
+  //     // creatOrFindStorage();
+  //   }
+  // }, [postData]);
+  // useEffect(() => {
+  //   if (isClickSubmit) {
+  //     // updateStorage();
+  //   }
+  // }, [postsList]);
   
   const postBtn = () => {
-    console.log(postDetail);
+    console.log(post);
     const isValid = formValidation();
     setIsCorrect(isValid);
     console.log(isCorrect);
     setIsClickSubmit(true);
-    setIsLocalStorageEmpty(true);
+    // setIsLocalStorageEmpty(true);
   };
 
-  const creatOrFindStorage = () => {
-    if ("postsList" in localStorage) {
-      setPostsList(JSON.parse(localStorage.getItem("postsList")));
-      setIsLocalStorageEmpty(false);
-    } else {
-      setPostsList(postsList.concat(postData));
-    }
-  };
+  // const creatOrFindStorage = () => {
+  //   if ("postsList" in localStorage) {
+  //     setPostsList(JSON.parse(localStorage.getItem("postsList")));
+  //     setIsLocalStorageEmpty(false);
+  //   } else {
+  //     setPostsList(postsList.concat(postData));
+  //   }
+  // };
 
+  // const updateStorage = () => {
+  //   console.log("Xxx");
+  //   if (isLocalStorageEmpty) {
+  //     localStorage.setItem("postsList", JSON.stringify(postsList));
+  //   } else {
+  //     var data = JSON.parse(localStorage.getItem("postsList"));
+  //     var updateData = [];
+  //     data.forEach((item) => {
+  //       updateData.push(item);
+  //     });
+  //     updateData.push(postData);
+  //           localStorage.setItem("postsList", JSON.stringify(updateData));
+  //     }
+  //     setPostSuccessMeg("Post send!!")
+  //     setTimeout(() => {
+  //       window.location.href = "allposts";
+  //     }, 1500);
+
+  //   }
   const updateStorage = () => {
-    console.log("Xxx");
-    if (isLocalStorageEmpty) {
-      localStorage.setItem("postsList", JSON.stringify(postsList));
-    } else {
-      var data = JSON.parse(localStorage.getItem("postsList"));
-      var updateData = [];
-      data.forEach((item) => {
-        updateData.push(item);
-      });
-      updateData.push(postData);
-            localStorage.setItem("postsList", JSON.stringify(updateData));
-      }
       setPostSuccessMeg("Post send!!")
       setTimeout(() => {
         window.location.href = "allposts";
